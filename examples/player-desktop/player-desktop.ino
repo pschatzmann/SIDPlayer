04cd43cefff737ffa0bf40f4c3674ee9e5994145
pschatzmann <phil.schatzmann@gmail.com>
2023-03-04 16:30:04 +0100

    Desktop build corrections

--- examples/player-desktop/player-desktop.ino
+++ examples/player-desktop/player-desktop.ino
@@ -3,7 +3,13 @@
  * @author Phil Schatzmann
  * @brief Sends the PCM stream of the the included demo SID files to the the standard output. 
  * You can compile and run this sketch on the desktop e.g. with player-desktop | aplay on linux 
- * Dependencies:
+ * To build it:
+ * go to this directory
+ * mkdir build
+ * cd build
+ * cmake ..
+ * make
+ * and execute it with ./player-desktop
  * @version 0.1
  * @date 2023-03-02
  * 
@@ -25,7 +31,11 @@ void setup() {
   AudioLogger::instance().begin(Serial, AudioLogger::Error);
 
   auto cfg = out.defaultConfig();
+  cfg.sample_rate = 44100;
+  cfg.channels = 2;
+  cfg.bits_per_sample = 16;
   out.begin(cfg);
+  wav.begin(cfg);
 
   // move to next song after playing for 5 minuts
   sid.setTimeout(60*5);
