cc931c1dd51f68d542dfe4138d5f57adb034097a
pschatzmann <phil.schatzmann@gmail.com>
2024-08-18 08:51:31 +0200

    doxygen

--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['selectstream_19',['selectStream',['../classaudio__tools_1_1SIDAudioSource.html#a433f8c90842dbc18140f64a86e15eac3',1,'audio_tools::SIDAudioSource::selectStream(int index)'],['../classaudio__tools_1_1SIDAudioSource.html#ad563fe8b0b233847e4565803235f71e8',1,'audio_tools::SIDAudioSource::selectStream(const char *path)']]],
   ['setactive_20',['setActive',['../classaudio__tools_1_1SIDPlayer.html#a603e30292b780485abbc8e27913fe980',1,'audio_tools::SIDPlayer']]],
-  ['setaudioinfo_21',['setAudioInfo',['../classaudio__tools_1_1SIDPlayer.html#a060d48f78682add19a3d69f61a81a588',1,'audio_tools::SIDPlayer']]],
+  ['setaudioinfo_21',['setAudioInfo',['../classaudio__tools_1_1SIDPlayer.html#af42f1921cc9fb74a732ed6a4a597592c',1,'audio_tools::SIDPlayer']]],
   ['setindex_22',['setIndex',['../classaudio__tools_1_1SIDPlayer.html#aaa51430de1b6e980e0f47f78f01c0b95',1,'audio_tools::SIDPlayer']]],
   ['setisplayingtimeout_23',['setIsPlayingTimeout',['../classaudio__tools_1_1SIDPlayer.html#ad28da4e93c8f8e2993ed763040f4fad9',1,'audio_tools::SIDPlayer']]],
   ['setpath_24',['setPath',['../classaudio__tools_1_1SIDPlayer.html#ae1366b2f33a4660e0bc9a148440df3a8',1,'audio_tools::SIDPlayer']]],
